47c1e9ffe9d24ba2b1e6e8aefc08a32fe4133d50
Camille Moutte <mouttecam@gmail.com>
2020-02-27 23:20:39 +0100

    init commit

---
+++ front-hephaistos/src/views/Login.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="login">
+    <b-card title="Login">
+      <span>{{ form.errorLogin }}</span>
+      <b-form @submit="onSubmit" v-if="true">
+        <b-form-group
+          id="input-group-email"
+          label="Email address:"
+          label-for="input-email"
+          description=""
+        >
+          <b-form-input
+            id="input-email"
+            v-model="form.email"
+            type="email"
+            required
+            placeholder="Enter email"
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group id="input-group-password" label="Your Password:" label-for="input-password">
+          <b-form-input
+            id="input-password"
+            v-model="form.password"
+            required
+            placeholder="Enter password"
+          ></b-form-input>
+        </b-form-group>
+
+        <b-button type="submit" variant="primary">Submit</b-button>
+      </b-form>
+    </b-card>
+    <b-card class="mt-3" header="Form Data Result">
+      <pre class="m-0">{{ form }}</pre>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      form: {
+        email: '',
+        password: '',
+        errorLogin: ''
+      },
+      show: true
+    }
+  },
+  methods: {
+    async onSubmit (evt) {
+      evt.preventDefault()
+      const { email, password } = this.form
+      try {
+        console.log('ee')
+        const result = await this.axios.post('/api/login', {
+          email,
+          password
+        })
+        this.user = result.data
+        this.loggedIn = true
+        alert(this.loggedIn)
+      } catch (err) {
+        this.form.errorLogin = 'Error your email or password are incorrect'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+  .login{
+    max-width: 1000px;
+    margin: auto;
+    text-align: left;
+  }
+  span{
+    color: red;
+  }
+</style>
